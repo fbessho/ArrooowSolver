9eaf1141c0a32c1acd8d9fea4693ea620ada623b
Fumihiro Bessho <iwabako@gmail.com>
2014-06-27 10:20:06 +0900

    Solve all stages in the Arrooow app

--- test/SolverTest.java
+++ test/SolverTest.java
@@ -1,3 +1,5 @@
+import com.google.common.base.Stopwatch;
+import com.gs.collections.impl.list.mutable.FastList;
 import com.gs.collections.impl.test.Verify;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,5 +24,65 @@ public class SolverTest
         List<Integer> sequence = new Solver().solve(board);
         Assert.assertNotNull(sequence);
         Verify.assertTrue(board.tapInSequence(sequence).isSolved());
+    }
+
+    /**
+     * Arroooowに入っている問題全てを解く
+     */
+    @Test
+    public void solveAll() throws Exception
+    {
+        List<String> stageList = FastList.newListWith(
+                /* stage  1 */  "rrrrrlllllrrrrrlllllrrrrr",
+                /* stage  2 */  "rrrrbtrrbbtttbbtttlbttlll",
+                /* stage  3 */  "rltttrttttrrtttrrrttrrrrt",
+                /* stage  4 */  "brbrbrtrtbrbrbbtrttbtlllb",
+                /* stage  5 */  "bbbbbbbbbbrrrrrtttttttttt",
+                /* stage  6 */  "rrrblbrbbbbtbbbbtbbbbtlll",
+                /* stage  7 */  "brbttbtbttttbttttbttrtrtl",
+                /* stage  8 */  "rrrrbrbbbbbllllbtttrrrrrr",
+                /* stage  9 */  "lblblllbbtlbbbtllbbttllrt",
+                /* stage 10 */  "lllllrbrrttbllltrrrbllbll",
+                /* stage 11 */  "rrrrbtlllbttllbbbllblllll",
+                /* stage 12 */  "trrbtttlbtttllttllltlrrrt",
+                /* stage 13 */  "lbbbbtbllltrrrbtltrbtllll",
+                /* stage 14 */  "tblllrrrbrtrbrtrtrrrlltll",
+                /* stage 15 */  "brbbbbllbtltbbbtllltrrbbl",
+                /* stage 16 */  "lbllrblbtlblbrtrbbrttrrtt",
+                /* stage 17 */  "tblbltbtrbtbtlltrrrbtllll",
+                /* stage 18 */  "ltlllrrrrbtlllbtlltllbtll",
+                /* stage 19 */  "lrrlllrllllrrlllrllllrrll",
+                /* stage 20 */  "rttbltllltbltltrrrrrbtltl",
+                /* stage 21 */  "tlblllrrrbtllllttbtbllllb",
+                /* stage 22 */  "rrrrbtlblllbbbrblbrbrbbbl",
+                /* stage 23 */  "blbtlbtbltbrrrrbtrrtrtrbt",
+                /* stage 24 */  "lllrtrrrtblrtllrtrrrtllll",
+                /* stage 25 */  "rrbrbrrrbbtrrrbttllbtlllb",
+                /* stage 26 */  "tlbrbtrbtbtrbbttrrrrtttlb",
+                /* stage 27 */  "brbrltrrttrtrtttrtbtrtbtt",
+                /* stage 28 */  "rrbbltlbrbrtbbltblrbtltll",
+                /* stage 29 */  "rrrrrtbllltbrrttbtbltltlt",
+                /* stage 30 */  "rrrrbtbblltlbrlblltlrrrrt",
+                /* stage 31 */  "tltbbrttbrlbrrbrrttbttlbr",
+                /* stage 32 */  "llllllrrrllrlrllrrrllllll",
+                /* stage 33 */  "trttttbbbttbrbttbbbtttttt",
+                /* stage 34 */  "brrlllrlrllrlrllrlrltrrrl",
+                /* stage 35 */  "rrrrlrrrlrrrlrrrlrrrlrrrr",
+                /* stage 36 */  "bblrlbltrtrtttlbllbltrtbt",
+                /* stage 37 */  "rtrlrtbllrrltltrlrbttrbtt",
+                /* stage 38 */  "rrrtbbrbtbrltlrbrttbrltbl",
+                /* stage 39 */  "rrtrbllbbbtrrbbrrtrbrtrtb",
+                /* stage 40 */  "lblrbtltrltrrbtbbtrblrrrl"
+        );
+
+        Stopwatch sw = Stopwatch.createUnstarted();
+        for (int i = 0; i < 40; i++) {
+            sw.start();
+            System.out.println(String.format("----------\nStage %d\n", i+1));
+            Solver.main(new String[]{stageList.get(i)});
+            sw.stop();
+            System.out.println("(" + sw + ")");
+            sw.reset();
+        }
     }
 }
