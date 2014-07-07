a0cc76b37bd5040073ad5877fce64426714b9825
Fumihiro Bessho <iwabako@gmail.com>
2014-07-08 05:51:12 +0900

    Enhance javadoc of the solver

--- src/Solver.java
+++ src/Solver.java
@@ -4,6 +4,33 @@ import java.util.List;
 
 /**
  * Solve Arrooow program
+ *
+ * It receives a problem as an argument of the main function and print an answer of it (the sequence to tap to solve
+ * the problem).
+ * An input of the program is a 25-character-length string. The 1st-5th characters represent the first row,
+ * the 6th - 10th do the second row, and so on, and each arrow is written as follows.
+ *   Right arrow: r (right)
+ *   Left arrow:  l (left)
+ *   Up arrow:    t (top)
+ *   Down arrow:  b (bottom)
+ *
+ * In the example below, the top row of the problem is composed by "-> -> v -> v", where -> is a right arrow and
+ * v is a down arrow, likewise the second row is "-> -> -> v v".
+ *
+ * If you tap the arrows at the position in the output below in the same order, every arrow disappears (solved).
+ *
+ * == Example usage ==
+ * java Solver rrbrbrrrbbtrrrbttllbtlllb
+ *
+ * == Example output ==
+ * [ 1] row: 1, column 1
+ * [ 2] row: 2, column 1
+ * [ 3] row: 3, column 2
+ * [ 4] row: 4, column 1
+ * [ 5] row: 4, column 4
+ * [ 6] row: 4, column 5
+ * [ 7] row: 5, column 2
+ * [ 8] row: 5, column 2
  */
 public class Solver
 {
